4d08ba46c28ed5260ce6c523892f35dfeb219e09
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-16 19:54:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,12 @@ def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody in line."
+  else
+    puts "Currently serving #{array[0]}"
+    array.shift
+  end
+end
