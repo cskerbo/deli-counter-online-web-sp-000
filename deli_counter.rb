8d5fd7c52002e5a0a797a66d72887a5add57497d
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-16 17:48:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,6 @@
 def line(array)
   array = []
   array.each_with_index {|person, number| <<}
-  if array.length < 1
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: #{array}."
+  puts array
   end
 end
